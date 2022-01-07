59654fd26e6202808c60178489238cc8741e49cc
Felipe Santos <rfelipe273@gmail.com>
2022-01-07 04:34:06 -0300

    feat: add tractors repository mongo implementation

--- src/infra/db/mongodb/tractors-repository-mongo.ts
+++ src/infra/db/mongodb/tractors-repository-mongo.ts
@@ -3,7 +3,10 @@ import { TractorModel } from "../../../domain/models";
 import { TractorsRepository } from "./../../../domain/repositories";
 export class TractorsRepositoryMongo implements TractorsRepository {
   async save(tractorDTO: Tractor): Promise<Tractor> {
-    const tractor = new TractorModel({ ...tractorDTO });
+    const tractor = new TractorModel({
+      name: tractorDTO.name,
+      image_base64: tractorDTO.image_base64,
+    });
     return await tractor.save();
   }
 }
