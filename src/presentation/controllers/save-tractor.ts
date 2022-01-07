0f6ea9c4c0901eaef563df92fed470e2261f7db0
Felipe Santos <rfelipe273@gmail.com>
2022-01-07 14:40:07 -0300

    feat: ensure save tractor controller response correct

--- src/presentation/controllers/save-tractor.ts
+++ src/presentation/controllers/save-tractor.ts
@@ -1,13 +1,19 @@
+import { HttpResponse, ok, serverError } from "./../protocols";
 import { SaveTractorService } from "./../../data/services/save-tractor";
 import { Controller } from "../protocols/controller";
 import { Tractor } from "./../../domain/models";
 
-export class SaveTractorController implements Controller<Tractor> {
+export class SaveTractorController implements Controller {
   constructor(
     private readonly tractor: Tractor,
     private readonly saveTractorService: SaveTractorService
   ) {}
-  async handle(): Promise<Tractor> {
-    return await this.saveTractorService.save(this.tractor);
+  async handle(): Promise<HttpResponse<Tractor>> {
+    try {
+      const res = await this.saveTractorService.save(this.tractor);
+      return ok(res);
+    } catch (err: any) {
+      return serverError(err);
+    }
   }
 }
