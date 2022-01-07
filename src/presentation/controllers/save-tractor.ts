39ef4448899b196d432a9c9d8e0e504c3a1b8598
Felipe Santos <rfelipe273@gmail.com>
2022-01-07 14:45:14 -0300

    fix: change to correct dependency injection

--- src/presentation/controllers/save-tractor.ts
+++ src/presentation/controllers/save-tractor.ts
@@ -1,12 +1,12 @@
 import { HttpResponse, ok, serverError } from "./../protocols";
-import { SaveTractorService } from "./../../data/services/save-tractor";
 import { Controller } from "../protocols/controller";
 import { Tractor } from "./../../domain/models";
+import { SaveTractor } from "../../domain/usecases";
 
 export class SaveTractorController implements Controller {
   constructor(
     private readonly tractor: Tractor,
-    private readonly saveTractorService: SaveTractorService
+    private readonly saveTractorService: SaveTractor
   ) {}
   async handle(): Promise<HttpResponse<Tractor>> {
     try {
