eedbc24fd58dff73576c5f62323c224d47098e28
Felipe Santos <rfelipe273@gmail.com>
2022-01-07 14:26:25 -0300

    feat: ensure SaveTractorService throws if image is not base 64

--- src/data/services/save-tractor.ts
+++ src/data/services/save-tractor.ts
@@ -1,12 +1,15 @@
 import { TractorsRepository } from "./../../domain/repositories";
 import { SaveTractor } from "../../domain/usecases";
 import { TractorDTO } from "../protocols";
+import { checkBase64 } from "../../helpers";
 
 export class SaveTractorService implements SaveTractor {
   constructor(private readonly tractorsRepository: TractorsRepository) {}
   async save(tractor: TractorDTO): Promise<TractorDTO> {
     if (!tractor.name || tractor.name === "")
       throw new Error("Tractor name is required");
+    if (tractor.image_base64 && !checkBase64(tractor.image_base64))
+      throw new Error("Image is not base 64");
     return await this.tractorsRepository.save(tractor);
   }
 }
