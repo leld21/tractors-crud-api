fd622382bc6d187b3bf00fd26252e6fd02819150
Felipe Santos <rfelipe273@gmail.com>
2022-01-10 03:22:44 -0300

    feat: add cors

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,13 @@
 import express, { Express } from "express";
 import bodyParser from "body-parser";
 
+const cors = require("cors");
+
 import save_tractor from "../routes/save-tractor";
 
 export const setupApp = (): Promise<Express> => {
   const app = express();
+  app.use(cors());
   app.use(bodyParser.json());
   app.use(save_tractor);
   return Promise.resolve(app);
